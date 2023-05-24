32feba6d60e2bdd198661843dddbf5d44b1c49c9
morses-code <morsescott88@gmail.com>
2023-05-24 10:20:37 +0100

    added gcp yaml and sound files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+interface SoundButtonProps {
+  soundFile: string;
 }
+
+const SoundButton: React.FC<SoundButtonProps> = ({ soundFile }) => {
+  const handleClick = () => {
+    const audio = new Audio(soundFile);
+    audio.play();
+  };
+
+  return (
+    <button className="sound-button" onClick={handleClick}>
+      💩
+    </button>
+  );
+};
+
+const AppHeader: React.FC = () => {
+  return <h1 className="app-header">Caleb's Fart Box</h1>;
+}
+
+const App: React.FC = () => {
+  const soundFiles = [
+    'sound1.mp3',
+    'sound2.mp3',
+    'sound3.mp3',
+    'sound4.mp3',
+    'sound5.mp3',
+    'sound6.mp3',
+  ];
+
+  return (
+    <div>
+      <div className="app-header-container"> 
+        <AppHeader />
+      </div>
+      <div className="app-container">
+        {soundFiles.map((soundFile, index) => (
+          <SoundButton key={index} soundFile={soundFile} />
+        ))}
+      </div>
+    </div>
+    
+  );
+};
 
 export default App;
